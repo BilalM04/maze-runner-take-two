65f85d30e67907982ed6c1d771e9f19f3a8ea8d0
bilalm04 <bilal71612@gmail.com>
2024-03-27 00:28:05 -0400

    added skeleton for BFS algo

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/algorithm/BFS.java
@@ -0,0 +1,33 @@
+package ca.mcmaster.se2aa4.mazerunner.algorithm;
+
+import ca.mcmaster.se2aa4.mazerunner.explorer.Explorer;
+import ca.mcmaster.se2aa4.mazerunner.explorer.MazeExplorer;
+import ca.mcmaster.se2aa4.mazerunner.maze.Maze;
+import ca.mcmaster.se2aa4.mazerunner.path.Path;
+import ca.mcmaster.se2aa4.mazerunner.position.Direction;
+import ca.mcmaster.se2aa4.mazerunner.position.Location;
+
+public class DFS implements MazeAlgorithm {
+    
+    public String getPath(Maze maze, boolean factorized) {
+        Location start = maze.findWestEntry();
+        start.setDirection(Direction.EAST);
+        Location end = maze.findEastEntry();
+        end.setDirection(Direction.EAST);
+        Path path = findPath(maze, start, end);
+
+        if (factorized) {
+            return path.getFactorizedPath();
+        } else {
+            return path.getCanonicalPath();
+        }
+    }
+
+    private Path findPath(Maze maze, Location start, Location end) {
+        Path path = new Path();
+        Explorer explorer = new MazeExplorer(maze, start);
+
+
+        return path;
+    }
+}
