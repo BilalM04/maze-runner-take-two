115038fead43739e76fd30c2fad0bf6644829f9f
bilalm04 <bilal71612@gmail.com>
2024-03-29 22:05:28 -0400

    created AlgoBenchmark skeleton

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/benchmark/AlgoBenchmark.java
@@ -0,0 +1,32 @@
+package ca.mcmaster.se2aa4.mazerunner.benchmark;
+
+import ca.mcmaster.se2aa4.mazerunner.configuration.Method;
+
+public class AlgoBenchmark implements Benchmark {
+    Method method;
+    Method baseline;
+
+    public AlgoBenchmark(Method method, Method baseline, String file) {
+
+    }
+    
+    public String generateReport() {
+        return "";
+    }
+
+    private double getLoadTime() {
+        return 0;
+    }
+
+    private double getMethodTime() {
+        return 0;
+    }
+
+    private double getBaselineTime() {
+        return 0;
+    }
+
+    private double getSpeedup() {
+        return 0;
+    }
+}
