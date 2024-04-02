5c33836bfd79638bc28386bd4c5df725a40fa56d
bilalm04 <bilal71612@gmail.com>
2024-04-01 23:11:13 -0400

    created tests for Path class close #26

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/PathTest.java
@@ -0,0 +1,50 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+
+import ca.mcmaster.se2aa4.mazerunner.path.Instruction;
+import ca.mcmaster.se2aa4.mazerunner.path.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PathTest {
+    private Path path;
+    
+    @BeforeEach
+    public void setUp() {
+        this.path = new Path();
+        path.addInstruction(Instruction.F);
+        path.addInstruction(Instruction.F);
+        path.addInstruction(Instruction.R);
+        path.addInstruction(Instruction.L);
+        path.addInstruction(Instruction.L);
+        path.addInstruction(Instruction.F);
+    }
+
+    @Test
+    public void canonicalTest() {
+        assertEquals("FF R LL F", path.getCanonicalPath());
+    }
+
+    @Test
+    public void factorizedTest() {
+        assertEquals("2F R 2L F ", path.getFactorizedPath());
+    }
+
+    @Test
+    public void getInstructionTest(){
+        assertEquals(Instruction.R, path.getInstruction(2));
+    }
+
+    @Test
+    public void appendTest(){
+        Path newPath = new Path();
+        newPath.addInstruction(Instruction.F);
+        newPath.addInstruction(Instruction.L);
+        path.appendPath(newPath);
+
+        assertEquals("2F R 2L 2F L ", path.getFactorizedPath());
+        assertEquals("FF R LL FF L", path.getCanonicalPath());
+    }
+}
